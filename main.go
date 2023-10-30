2c9918abf9716f92a55837499558850b9f252f46
hylkest <76428093+hylkest@users.noreply.github.com>
2023-10-30 13:23:24 +0100

    Added webscraper

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import f "fmt"
+import (
+	"fmt"
+	"log"
+	"github.com/gocolly/colly"
+)
 
-func main()  {
-	f.Println("test")
+func main() {
+	c := colly.NewCollector()
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Request URL: %v\nFailed with response: %v\nError: %v\n", r.Request.URL, r, err)
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Println(link)
+	})
+
+	err := c.Visit("https://cssight.com")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
